8fbfb4241ddf54e48d39b467e0131d8a8aaaed1d
Emilie MOLINA PARRA <B269CP@login.axa>
2023-01-09 00:28:29 +0100

    maj dec 2022

---
+++ BlazorApp_ProjetCSharp/Shared/Arbre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp_ProjetCSharp.Shared
+{
+    internal abstract class Arbre
+    {
+        // Attributs arbre
+        private readonly int IdArbre;
+        private double Position;
+        private int LargeurTronc;
+        private int HauteurTronc;
+        private int LargeurFeuille;
+        private int HauteurFeuille;
+    }
+}
